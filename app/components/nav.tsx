001ed365f35ff283b6bcf45bbedef0afdd88fff9
Sanuthi Liyoni <sanuthi.leo0909@gmail.com>
2026-07-29 11:00:55 +0530

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,9 +7,6 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
 }
 
 export function Navbar() {
@@ -26,7 +23,8 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all flex align-middle relative py-1 px-2 m-1 font-medium"
+                  style={{ color: 'var(--ink)' }}
                 >
                   {name}
                 </Link>
