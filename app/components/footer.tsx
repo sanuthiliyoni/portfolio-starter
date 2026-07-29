0881bd07f1850fb885a4455b7ec8ef51fec42fae
Sanuthi Liyoni <sanuthi.leo0909@gmail.com>
2026-07-29 11:02:45 +0530

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,10 +18,14 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+      <ul
+        className="font-sm mt-8 flex flex-col space-x-0 space-y-2 md:flex-row md:space-x-4 md:space-y-0"
+        style={{ color: 'var(--muted)' }}
+      >
+        <li
+          
+            className="flex items-center transition-all"
+            style={{ color: 'var(--muted)' }}
             rel="noopener noreferrer"
             target="_blank"
             href="/rss"
@@ -30,31 +34,32 @@ export default function Footer() {
             <p className="ml-2 h-7">rss</p>
           </a>
         </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+        <li
+          
+            className="flex items-center transition-all"
+            style={{ color: 'var(--muted)' }}
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/sanuthiliyoni"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+        <li
+            className="flex items-center transition-all"
+            style={{ color: 'var(--muted)' }}
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://www.linkedin.com/in/sanuthiliyoni/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+      <p className="mt-8" style={{ color: 'var(--muted)' }}>
+        © {new Date().getFullYear()} Sanuthi Liyoni Rathnayake
       </p>
     </footer>
   )
