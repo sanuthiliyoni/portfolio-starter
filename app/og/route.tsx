1d01554236f06995e6846341e76ac9b831d41a5e
Sanuthi Liyoni <sanuthi.leo0909@gmail.com>
2026-07-29 10:29:47 +0530

    preview image
    generates the preview image when your link gets shared

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,13 +2,18 @@ import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
   let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+  let title = url.searchParams.get('title') || 'Sanuthi Liyoni Rathnayake'
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+      <div
+        tw="flex flex-col w-full h-full items-center justify-center"
+        style={{ backgroundColor: '#FAF7F0' }}>
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+          <h2
+            tw="flex flex-col text-5xl font-bold tracking-tight text-left"
+            style={{ color: '#2D7D6E' }}
+          >
             {title}
           </h2>
         </div>
